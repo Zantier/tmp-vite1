374251bbf1bdecdc81b67c6351aba4a75c15d2a0
David Phillips <zantier.tasa@gmail.com>
2024-03-10 23:40:08 +0000

    Change base URL to match the URL in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/tmp-vite1/',
   build: {
     // Prevent small assets being inlined as base64
     assetsInlineLimit: 0,
