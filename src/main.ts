e0824dac6e990c738dea8633d6afae9d485f0333
David Phillips <zantier.tasa@gmail.com>
2024-03-10 23:38:23 +0000

    Make code work with differing base URL (for github pages)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import './style.css'
 import data from './mydata.json'
 import data_url from './mydata.json?url'
 
+console.log(import.meta.env.BASE_URL);
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    This page is /. <a href="/main2/">Go to /main2/</a>
+    This page is /. <a href="${import.meta.env.BASE_URL}main2/">Go to /main2/</a>
   </div>
   <div>
     The frog data is: ${data.frogs.join(', ')}
--- src/main2.ts
+++ src/main2.ts
@@ -4,7 +4,7 @@ import data_url from './mydata.json?url'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    This page is /main2/. <a href="/">Go to /</a>
+    This page is /main2/. <a href="${import.meta.env.BASE_URL}">Go to /</a>
   </div>
   <div>
     The frog data is: ${data.frogs.join(', ')}
