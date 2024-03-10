90269e4495a8221090c598aace92cf844f694689
David Phillips <zantier.tasa@gmail.com>
2024-03-10 23:40:50 +0000

    tmp

---
+++ docs/assets/main-BmYBun_k.js
@@ -0,0 +1,11 @@
+import{d as i,a}from"./mydata-cAVU7OdG.js";console.log("/tmp-vite1/");document.querySelector("#app").innerHTML=`
+  <div>
+    This page is /. <a href="/tmp-vite1/main2/">Go to /main2/</a>
+  </div>
+  <div>
+    The frog data is: ${i.frogs.join(", ")}
+  </div>
+  <div>
+    The JSON URL is: ${a}
+  </div>
+`;
---
+++ docs/assets/nested-Bf6iLKlG.js
@@ -0,0 +1,11 @@
+import{d as i,a}from"./mydata-cAVU7OdG.js";document.querySelector("#app").innerHTML=`
+  <div>
+    This page is /main2/. <a href="/tmp-vite1/">Go to /</a>
+  </div>
+  <div>
+    The frog data is: ${i.frogs.join(", ")}
+  </div>
+  <div>
+    The JSON URL is: ${a}
+  </div>
+`;
